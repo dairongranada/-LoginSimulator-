7b713053ea2f214fba318e14e1158f5b86931545
Dairon Camargo Granada <93607018+dairongranada@users.noreply.github.com>
2022-05-07 11:52:43 -0500

    Simulador De login Con (JS). DG

--- js/animacion.js
+++ js/animacion.js
@@ -1,28 +1,4 @@
 
-// MENU HAMBURGUESA 
-
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
-
-
-
-
 window.sr =ScrollReveal();
 
 //      I N D E X 
@@ -34,9 +10,29 @@ sr.reveal('.BarraNav',{
 
 sr.reveal('.Contenedor-Principal',{duration: 1200,});
 sr.reveal('.ContenedorCartas',{duration: 1200,});
-sr.reveal('.ContenedorCartas2',{duration: 1200,});
+sr.reveal('.ContenedorCartas2',{duration: 1200,}); //    I N D E X
 sr.reveal('.BackImage3',{duration: 1200,});
 sr.reveal('.social',{duration: 1200,});
 
-//    L O G I N   A N D   R E G I S T E R
-sr.reveal('.bodyFomularios',{duration: 1200,});
+sr.reveal('.bodyFomularios',{duration: 1200,}); //    L O G I N   A N D   R E G I S T E R
+
+
+
+//       M E N U   H A M B U R G U E S A 
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
